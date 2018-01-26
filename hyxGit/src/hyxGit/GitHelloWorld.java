f3e12b49c28762d4fefc3041820067e403573b26
huangyuxuan <huangyuxuan@edenep.net>
2018-01-26 15:06:06 +0800

    update

--- hyxGit/src/hyxGit/GitHelloWorld.java
+++ hyxGit/src/hyxGit/GitHelloWorld.java
@@ -3,6 +3,10 @@ package hyxGit;
 public class GitHelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("This is my Git,hello World!");
+		System.out.println(explain());
+	}
+	
+	private static String explain(){
+		return "This is my Git,hello World!";
 	}
 }
